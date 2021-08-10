873166eb38300110fa38d2cd8afb0e640086be94
alters-mit <alters@mit.edu>
2021-08-10 11:38:29 -0400

    floorplan movement variance

--- controllers/tests/movement_variance.py
+++ controllers/tests/movement_variance.py
@@ -1,7 +1,7 @@
-from typing import List
+from typing import List, Union
 import numpy as np
-from tdw.tdw_utils import TDWUtils
 from magnebot import Magnebot, ActionStatus
+from magnebot.collision_detection import CollisionDetection
 
 
 class MovementVariance(Magnebot):
@@ -11,34 +11,32 @@ class MovementVariance(Magnebot):
     This controller can be used to determine how much the Magnebot API varies between machines.
     """
 
-    # This is a pre-calculated path that the Magnebot will use to move between rooms.
-    PATH: np.array = np.array([[6.396355, 0, -2.465405],
-                               [5.41636, 0, -1.4854207],
-                               [4.615, 0, -0.9954208],
-                               [3.946356, 0, 0.66],
-                               [0.4, 0, 0.66],
-                               [0.02635, 0, -1.975]])
+    def __init__(self, port: int = 1071):
+        self.positions: List[np.array] = list()
+        super().__init__(port=port)
 
-    def init_scene(self) -> ActionStatus:
-        return self._init_scene(scene=[{"$type": "load_scene", "scene_name": "ProcGenScene"},
-                                       TDWUtils.create_empty_room(20, 20)])
+    def move_by(self, distance: float, arrived_at: float = 0.1, stop_on_collision: Union[bool, CollisionDetection] = True) -> ActionStatus:
+        status = super().move_by(distance=distance, arrived_at=arrived_at, stop_on_collision=stop_on_collision)
+        self.positions.append(self.state.magnebot_transform.position)
+        return status
 
     def run(self, write: bool = False) -> None:
-        self.init_scene()
-        positions: List[np.array] = [self.state.magnebot_transform.position]
-        for waypoint in MovementVariance.PATH:
-            self._next_frame_commands.append({"$type": "add_position_marker",
-                                              "position": TDWUtils.array_to_vector3(waypoint)})
-            self.move_to(TDWUtils.array_to_vector3(waypoint))
-            positions.append(self.state.magnebot_transform.position)
+        self.init_floorplan_scene(scene="1b", layout=1, room=4)
+        self.move_to(target={"x": 9.976, "y": 0, "z": -0.97}, stop_on_collision=False, arrived_at=0.2)
+        self.move_to(target={"x": 12.22, "y": 0, "z": -0.97}, stop_on_collision=False, aligned_at=1)
+        self.move_to(target={"x": 11.46, "y": 0, "z": -1.11}, stop_on_collision=False)
+        self.move_to(target={"x": 9.945, "y": 0, "z": -1.11}, stop_on_collision=False)
+        self.move_by(2, stop_on_collision=False)
+        self.move_by(-3, stop_on_collision=False)
         self.end()
+
         # Write the canonical positions to disk.
         if write:
-            np.save("movement_variance", np.array(positions))
+            np.save("movement_variance", np.array(self.positions))
         else:
             canonical_positions = np.load("movement_variance.npy")
             diffs = []
-            for position, canonical_position in zip(positions, canonical_positions):
+            for position, canonical_position in zip(self.positions, canonical_positions):
                 diff = np.linalg.norm(position - canonical_position)
                 diffs.append(diff)
                 print(diff)
