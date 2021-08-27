bd8e6e37f5696351f37468ff792912f19ee1f32a
alters-mit <alters@mit.edu>
2021-08-27 11:28:37 -0400

    Fixed: wrong ikpy version installed

--- magnebot/constants.py
+++ magnebot/constants.py
@@ -3,4 +3,4 @@ MAGNEBOT_RADIUS: float = 0.22
 # The cell size on the occupancy map. This is slightly bigger than diameter of the Magnebot.
 OCCUPANCY_CELL_SIZE: float = (MAGNEBOT_RADIUS * 2) + 0.05
 # The required version of TDW.
-TDW_VERSION: str = "1.8.24"
+TDW_VERSION: str = "1.8.25"
